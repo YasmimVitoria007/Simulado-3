49db255832fe70112b00cea6ee3100a031291e54
YasmimVitoria007 <vitoriiiayasmim@gmail.com>
2026-05-05 12:58:00 -0300

    Atualizando projeto

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Livewire\Movimentacao\MovimentacaoCreate;
+use App\Livewire\Movimentacao\MovimentacaoIndex;
 use App\Livewire\Produto\ProdutoCreate;
 use App\Livewire\Produto\ProdutoEdit;
 use App\Livewire\Produto\ProdutoIndex;
@@ -8,3 +10,6 @@ use Illuminate\Support\Facades\Route;
 route::get('/produto/create', ProdutoCreate::class)->name('produto.create');
 route::get('/produto/edit/{id}', ProdutoEdit::class)->name('produto.edit');
 route::get('/produto', ProdutoIndex::class)->name('produto.index');
+
+route::get('/movimentacao', MovimentacaoCreate::class)->name('movimentacao.create');
+route::get('/movimentacao/index', MovimentacaoIndex::class)->name('movimentacao.index');
